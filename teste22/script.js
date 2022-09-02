ea8d0f6f0a9bf751232da61a571d24d113f1f030
Reinaldo <ruanpinheirolima2003@gmail.com>
2022-09-02 10:18:37 -0300

    Create teste22
    Calculador numérico com JS.

---
+++ teste22/script.js
@@ -0,0 +1,18 @@
+function Clicar(){
+    var n1 = document.getElementById ('one')
+    var n2 = document.getElementById ('two')
+    var n5 = document.getElementsByName ('operator')
+
+    var $n1 = Number(n1.value)
+    var $n2 = Number(n2.value)
+
+    if(n5[0].checked){
+        resultado.innerHTML = $n1 + $n2
+    } else if(n5[1].checked){
+        resultado.innerHTML = $n1 - $n2
+    } else if(n5[2].checked){
+        resultado.innerHTML = $n1 * $n2
+    } else if (n5[3].checked){
+        resultado.innerHTML = $n1 / $n2
+    }
+}
