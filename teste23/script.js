afaef5712b7e54b96b15244c51b3e9ba2c85f600
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2022-09-29 15:21:01 -0300

    Update script.js

--- teste23/script.js
+++ teste23/script.js
@@ -1,13 +1,24 @@
-function Verificar(){
+function Verificar() {
     let $name = document.querySelector('#name')
     let $lastName = document.querySelector('#Lastname')
     let $email = document.querySelector('#email')
     let $password = document.querySelector('#password')
-    let $foot = document.querySelector('.foot')
+    let $form = document.getElementById('formulario')
 
-    if(
-        $name.value.length === 0
-    ){
-        $foot.innerHTML += '<p>ERRO</p>'
-    }
+    if (
+        $name.value.length === 0 ||
+        $lastName.value.length === 0 ||
+        $email.value.length === 0 ||
+        $password.value.length === 0
+
+
+    ) {
+        alert('[ERRO] Verifique se todos os campos estão preenchidos.')
+        $form.addEventListener('submit', (event) => {
+            event.preventDefault()
+        })
+    } else{
+        alert('Formulário enviado com sucesso!')
+        $form.reset()
+        }
 }
