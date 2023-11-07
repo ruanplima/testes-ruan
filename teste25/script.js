66591ca66215253ac3dc73227e3cecfe9524c37a
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2023-11-07 17:26:04 -0300

    Update JS

--- teste25/script.js
+++ teste25/script.js
@@ -1,5 +1,10 @@
 const password = document.querySelector('input[type="password"]')
 const icon = document.querySelector('.fa-eye')
+const formulario = document.querySelector('form')
+const btn = document.querySelector('button')
+const nome = document.querySelector('input[type="text"]')
+const email = document.querySelector('input[type="email"]')
+
 
 function mostrarOcultarSenha(){
     if(password.type == 'password'){
@@ -11,5 +16,22 @@ function mostrarOcultarSenha(){
     }
 }
 
+function enviarForm(){
+    if(password.value.length == '' || nome.value.length == ''
+     || email.value.length == ''){
+        alert('[ERRO] Preencha todos os campos corretamente')
+        
+    } else{
+        password.removeAttribute('required')
+        nome.removeAttribute('required')
+        email.removeAttribute('required')
+        nome.value = ''
+        password.value = ''
+        email.value = ''
+        alert('Cadrasto realizado com sucesso')
+    }
+}
 
+
+btn.addEventListener('click', enviarForm)
 icon.addEventListener('click', mostrarOcultarSenha)
